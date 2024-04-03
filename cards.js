83816c98701d16aa7e1d3b388b49a1eea5d5a850
Kruti Rathod <swapsmagic83@gmail.com>
2024-04-03 15:43:20 -0700

    editing files

--- cards.js
+++ cards.js
@@ -1,47 +1,28 @@
-$(function() {
-    let baseURL = 'https://deckofcardsapi.com/api/deck';
-  
-    // 1.
-    async function part1() {
-      let data = await $.getJSON(`${baseURL}/new/draw/`);
-      let { suit, value } = data.cards[0];
-      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-    }
-  
-    // 2.
-    async function part2() {
-      let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
-      let deckId = firstCardData.deck_id;
-      let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
-      [firstCardData, secondCardData].forEach(card => {
-        let { suit, value } = card.cards[0];
-        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-      });
-    }
-  
-    // 3.
-    async function setup() {
-      let $btn = $('button');
-      let $cardArea = $('#card-area');
-  
-      let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
-      $btn.show().on('click', async function() {
-        let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
-        let cardSrc = cardData.cards[0].image;
-        let angle = Math.random() * 90 - 45;
-        let randomX = Math.random() * 40 - 20;
-        let randomY = Math.random() * 40 - 20;
-        $cardArea.append(
-          $('<img>', {
-            src: cardSrc,
-            css: {
-              transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-            }
-          })
-        );
-        if (cardData.remaining === 0) $btn.remove();
-      });
-    }
-    setup();
-  });
-  
+async function singleCard(){
+  response = await axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+  console.log(`${response.data.cards[0].value} of ${response.data.cards[0].suit}`)
+}
+singleCard()
+
+async function doubleCard(){
+  response1 = await axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+  let deckId=response1.data.deck_id
+  console.log(`${response1.data.cards[0].value} of ${response1.data.cards[0].suit}`)
+  response2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+  console.log(`${response2.data.cards[0].value} of ${response2.data.cards[0].suit}`)
+}
+doubleCard()
+
+let deckId;
+let btn= $('.button')
+let textarea =$('#card-area')
+let imagesrc = $('.ImageSrc')
+async function drawMultipleCards(){
+  response = await axios.get('https://deckofcardsapi.com/api/deck/new/')
+  deckId = response.data.deck_id;  
+}
+async function clickButton(){
+  response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+  document.getElementById("imageid").src = response.data.cards[0].image
+}
+drawMultipleCards()
--- nums.js
+++ nums.js
@@ -1,28 +1,43 @@
-let favNumber = 5;
-let baseURL = "http://numbersapi.com";
-
-// 1.
-async function part1() {
-  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
-  console.log(data);
+async function getFact(number) {
+  try {
+      response = await axios.get(`http://numbersapi.com/${number}`)
+      console.log(response.data)
+      $(".1").append(`<p>${response.data}</p>`)
+  }
+  catch(e) {
+      console.error(e)
+  }
 }
-part1();
 
-// 2.
-const favNumbers = [7, 11, 22];
-async function part2() {
-  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
-  console.log(data);
-}
-part2();
+getFact(10)
 
-// 3.
-async function part3() {
-  let facts = await Promise.all(
-    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
-  );
-  facts.forEach(data => {
-    $('body').append(`<p>${data.text}</p>`);
-  });
+async function getTwoFact(numbers){
+  try {
+      for (let number of numbers){
+      response= await axios.get(`http://numbersapi.com/${number}`)
+      console.log(response.data)
+      $(".2").append(`<p>${response.data}</p>`)
+      }
+  }
+  catch(e){
+      console.error(e)
+  }
 }
-part3();
+getTwoFact([23,24,25,27])
+
+async function multipleFact(number){
+  res1= await axios.get(`http://numbersapi.com/${number}`)
+  console.log(res1.data)
+  $(".3").append(`<p>${res1.data}</p>`)
+  res2=await axios.get(`http://numbersapi.com/${number}`)
+  console.log(res2.data)
+  $(".4").append(`<p>${res2.data}</p>`)
+  res3=await axios.get(`http://numbersapi.com/${number}`)
+  console.log(res3.data)
+  $(".5").append(`<p>${res3.data}</p>`)
+  res4=await axios.get(`http://numbersapi.com/${number}`)
+  console.log(res4.data)
+  $(".6").append(`<p>${res4.data}</p>`)
+
+}
+multipleFact(35)
